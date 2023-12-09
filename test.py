cead5d5c6eb3f06bdc58935b8a4a690b2cb5d3ce
FD-Bean <jing_bin1996@hotmail.com>
2023-12-09 18:45:48 +0800

    added libraries

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
 import pandas as pd
 from sspipe import px, p
+import Ashare.Ashare as as_util
+import adata as ad_util
+import MyTT.MyTT as tt_util
 
-print(pd.DataFrame([[1,2,3,4], [5,6,7,8]], columns=['a','b','c','d']) | px.drop('a',axis=1))
+res_df = ad_util.stock.info.all_code()
+print(res_df)
+
+
+df = as_util.get_price('sh000001', frequency='1d', count=1000)
+print('上证指数日线行情\n', df)
